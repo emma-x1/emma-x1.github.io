bdc6691605bc72013b52baa5b98789c1d91068fe
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2025-12-23 15:42:08 -0500

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { mdsvex } from 'mdsvex'
@@ -19,8 +19,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			fallback: '404.html'
+		})
+	},
 };
+
+config.paths = { base: process.hasUncaughtExceptionCaptureCallback.includes('dev') ? '' : '/emma-x1.github.io'}
 
 export default config;
