5c93e9644a34f4ec1dbc6361678234bb8e557ce7
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2025-12-23 16:41:30 -0500

    disable ssr

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 import { mdsvex } from 'mdsvex'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
 }
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
