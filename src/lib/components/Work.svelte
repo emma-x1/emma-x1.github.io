cc6748106594413a6c8ba909129aa6fb23fd5893
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2025-12-26 13:47:10 -0500

    update desc

--- src/lib/components/Work.svelte
+++ src/lib/components/Work.svelte
@@ -6,8 +6,10 @@
 <p>I'm lucky to have learned from the talented teams at each of my roles. Here's where I've contributed to shareholder value - see <a href="https://www.linkedin.com/in/emmaxing/"> Linkedin </a> for more.</p>
 <section class="timeline">
     <WorkComponent name="Incoming @TextQL" title="Intern of Technical Staff" time="Jan. 2026 - Apr. 2026" desc=""/>
-    <WorkComponent name="RBC Borealis" title="Machine Learning Software Engineer" time="Sept. 2025 - Dec. 2025" desc="Delivered machine learning models for credit adjudication."/>
-    <WorkComponent name="RBC" title="Full Stack Developer" time="May. 2025 - Aug. 2025" desc=""/>
+    <WorkComponent name="RBC Borealis" title="Machine Learning Software Engineer" time="Sept. 2025 - Dec. 2025" 
+    desc="Delivered machine learning models for credit adjudication."/>
+    <WorkComponent name="RBC" title="Full Stack Developer" time="May. 2025 - Aug. 2025" 
+    desc="Worked on asynchronous messaging channels to evaluate and optimize chatbots."/>
 </section>
 
 <style>
