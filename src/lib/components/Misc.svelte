3fb951b367cabcba324c5001004306dcc01260e9
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2026-03-09 02:16:39 -0400

    links

--- src/lib/components/Misc.svelte
+++ src/lib/components/Misc.svelte
@@ -1,6 +1,6 @@
 <h2 id="misc">Etc.</h2>
 <p>There's a lot to love outside of tech.</p>
-<li>My favourite way to destress is to run. I ran the Toronto Half Marathon this year, and my current <a href="https://www.strava.com/athletes/68351646">Strava</a> streak is about 35 weeks long.</li>
+<li>My favourite way to destress is to run. I ran the Toronto Half Marathon last year, and my current <a href="https://www.strava.com/athletes/68351646">Strava</a> streak is about 35 weeks long.</li>
 <li>If I could, I'd read all the time. I consume writing voraciously - whether that's <a href="https://www.goodreads.com/user/show/185891057-emma">novels</a>, blog posts, or Substack articles.</li>
 <li>I dabble in baking - I've made cookies, puff pastry, and churros.</li>
 <li>Cultivating a vibrant builder ecosystem is important to me. That's why I work on Hack The North - last year, I organized mentorship and activities and helped build our CTF challenge. This year, I'm working on Techyon.</li>
