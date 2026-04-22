c9cac9e80e7d3119f1d053557b72efe355ececa2
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2026-04-21 23:32:18 -0400

    link

--- src/lib/components/Misc.svelte
+++ src/lib/components/Misc.svelte
@@ -1,7 +1,7 @@
 <h2 id="misc">Etc.</h2>
 <p>There's a lot to love outside of tech.</p>
 <li>My favourite way to destress is to run. I've run the Toronto Waterfront Half Marathon and Brooklyn Half Marathon, and my current <a href="https://www.strava.com/athletes/68351646">Strava</a> streak is about 52 weeks long.</li>
-<li>If I could, I'd read all the time. I consume writing voraciously - whether that's <a href="https://www.goodreads.com/user/show/185891057-emma">novels</a>, blog posts, or Substack articles.</li>
+<li>If I could, I'd read all the time. I consume writing voraciously - whether that's <a href="https://www.goodreads.com/user/show/185891057-emma">novels</a>, blog posts, or <a href="https://emm4x.substack.com/">Substack</a> articles.</li>
 <li>I dabble in baking - I've made cookies, puff pastry, and churros.</li>
 <li>Cultivating a vibrant builder ecosystem is important to me. That's why I work on Hack The North - last year, I organized mentorship and activities and helped build our CTF challenge. This year, I'm working on Techyon.</li>
 <li>Empowering beginners in tech allows us to inspire more talented engineers, and I've worked in support of this mission by leading Hack404, Celestial, and Solstice - three beginner-friendly hackathons.</li>
